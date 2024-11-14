48b4f94cfa74b029ea5ed091dc229710f8cb89bd
KartikeyJoshi916 <95592393+KartikeyJoshi916@users.noreply.github.com>
2024-11-14 18:52:59 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,15 @@
 //your JS code here. If required.
+function daysOfAYear(years){
+	if(years%4==0){
+		if(years%100==0){
+			if(years%400==0){
+				return 366;
+			}
+			else{
+				return 365;
+			}
+		}
+		return 366;
+	}
+	else return 365;
+}
